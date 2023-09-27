e69ed89f5a7344c1f79b83cf439cf8c02aebc030
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-09-27 17:55:58 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Hayder"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let upper = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upper}!`
+let firstChar = currentUser[0];
+const shortGreeting = `Welcome, ${firstChar}!`
